fb141f9f4a3d46b7eda382eedd48fbe20fe08672
Anthony McKeever <snip3rm00n@gmail.com>
2020-04-16 20:18:52 -0700

    Add blogging functionality
    Add and migrate categories
    Add blogging views
    Add templates for new views
    Add login/logout flow
    Add CSS
    Add Tests
    Update base to use static files
    Update base to display header (nav bar)

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
 
-# Create your views here.
+from blogging.models import Post
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by("-published_date")
+    context = {"posts": posts}
+
+    return render(request, "blogging/list.html", context)
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except:
+        raise Http404
+    context = {"post": post}
+    return render(request, "blogging/detail.html", context)
